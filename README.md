913ff76bfb59b8567aabeae6a879deb7ea7e99a7
Kian-Meng Ang <kianmeng@cpan.org>
2021-07-02 00:29:13 +0800

    Misc doc changes
    Besides other documentation changes, this commit ensures the generated
    HTML doc for HexDocs.pm will become the source of truth for this Elixir
    library and leverage on latest features of ExDoc.

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Kerosene
 
+[![Module Version](https://img.shields.io/hexpm/v/kerosene.svg)](https://hex.pm/packages/kerosene)
+[![Hex Docs](https://img.shields.io/badge/hex-docs-lightgreen.svg)](https://hexdocs.pm/kerosene/)
+[![Total Download](https://img.shields.io/hexpm/dt/kerosene.svg)](https://hex.pm/packages/kerosene)
+[![License](https://img.shields.io/hexpm/l/kerosene.svg)](https://github.com/elixirdrops/kerosene/blob/master/LICENSE.md)
+[![Last Updated](https://img.shields.io/github/last-commit/elixirdrops/kerosene.svg)](https://github.com/elixirdrops/kerosene/commits/master)
+
 Pagination for Ecto and Phoenix.
 
 
@@ -7,17 +13,21 @@ Pagination for Ecto and Phoenix.
 
 The package is [available in Hex](https://hex.pm/packages/kerosene), the package can be installed as:
 
-Add kerosene to your list of dependencies in `mix.exs`:
+Add `:kerosene` to your list of dependencies in `mix.exs`:
+
 ```elixir
 def deps do
-  [{:kerosene, "~> 0.9.0"}]
+  [
+    {:kerosene, "~> 0.9.0"}
+  ]
 end
 ```
 
-Add Kerosene to your `repo.ex`:
+Add `:kerosene` to your `repo.ex`:
+
 ```elixir
 defmodule MyApp.Repo do
-  use Ecto.Repo, 
+  use Ecto.Repo,
     otp_app: :testapp,
     adapter: Ecto.Adapters.Postgres
   use Kerosene, per_page: 2
@@ -25,10 +35,12 @@ end
 ```
 
 ## Usage
-Start paginating your queries 
+
+Start paginating your queries:
+
 ```elixir
 def index(conn, params) do
-  {products, kerosene} = 
+  {products, kerosene} =
   Product
   |> Product.with_lowest_price
   |> Repo.paginate(params)
@@ -37,7 +49,8 @@ def index(conn, params) do
 end
 ```
 
-Add view helpers to your view 
+Add view helpers to your view:
+
 ```elixir
 defmodule MyApp.ProductView do
   use MyApp.Web, :view
@@ -45,24 +58,27 @@ defmodule MyApp.ProductView do
 end
 ```
 
-Generate the links using the view helpers
+Generate the links using the view helpers:
+
 ```elixir
 <%= paginate @conn, @kerosene %>
 ```
 
-Kerosene provides a [list ](https://hexdocs.pm/kerosene/Kerosene.HTML.html#__using__/1) of themes for pagination. By default it uses bootstrap. To use some other, add to config/config.exs:
+Kerosene provides a [list ](https://hexdocs.pm/kerosene/Kerosene.HTML.html#__using__/1) of themes for pagination. By default it uses bootstrap. To use some other, add to `config/config.exs`:
+
 ```elixir
 config :kerosene,
 	theme: :foundation
 ```
 
 If you need reduced number of links in pagination, you can use `simple mode` option, to display only Prev/Next links:
+
 ```elixir
 config :kerosene,
 	mode:  :simple
 ```
 
-Building apis or SPA's, no problem Kerosene has support for Json.
+Building APIs or SPA's, no problem Kerosene has support for JSON.
 
 ```elixir
 defmodule MyApp.ProductView do
@@ -83,15 +99,13 @@ defmodule MyApp.ProductView do
 end
 ```
 
-
 You can also send in options to paginate helper look at the docs for more details.
 
 ## Contributing
-	
+
 Please do send pull requests and bug reports, positive feedback is always welcome.
 
-
-## Acknowledgement
+## Acknowledgment
 
 I would like to Thank
 
@@ -99,6 +113,9 @@ I would like to Thank
 * Drew Olson (@drewolson)
 * Akira Matsuda (@amatsuda)
 
-## License
+## Copyright and License
 
-Please take a look at LICENSE.md
+Copyright (c) 2014 Ally Raza
+
+Released under the MIT License, which can be found in the repository in
+[LICENSE.md](./LICENSE.md).
--- mix.exs
+++ mix.exs
@@ -1,70 +1,89 @@
 defmodule Kerosene.Mixfile do
   use Mix.Project
+
+  @source_url "https://github.com/elixirdrops/kerosene"
   @version "0.9.0"
 
   def project do
-    [app: :kerosene,
-     version: @version,
-     elixir: "~> 1.2",
-     elixirc_paths: path(Mix.env),
-     package: package(),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     aliases: aliases(),
-     name: "Kerosene",
-     docs: [source_ref: "v#{@version}", main: "Kerosene"],
-     source_url: "https://github.com/elixirdrops/kerosene",
-     description: """
-     Pagination for Ecto and Phoenix.
-     """]
+    [
+      app: :kerosene,
+      version: @version,
+      elixir: "~> 1.2",
+      name: "Kerosene",
+      elixirc_paths: path(Mix.env()),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      package: package(),
+      deps: deps(),
+      docs: docs(),
+      aliases: aliases(),
+      preferred_cli_env: preferred_cli_env()
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: application(Mix.env)]
+    [
+      applications: application(Mix.env())
+    ]
   end
+
   defp application(:test), do: [:postgrex, :ecto, :logger]
   defp application(_), do: [:logger]
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:phoenix_html, "~> 2.10"},
-     {:plug, "~> 1.4"},
-     {:ecto, "~> 3.0"},
-     {:ecto_sql, "~> 3.0"},
-     # Test dependencies
-     {:postgrex, "~> 0.14.0", only: [:test]},
-     # Docs dependencies
-     {:earmark, "~> 0.1", only: :docs},
-     {:ex_doc, "~> 0.11", only: :docs},
-     {:inch_ex, "~> 0.2", only: :docs}]
+    [
+      {:ecto, "~> 3.0"},
+      {:ecto_sql, "~> 3.0"},
+      {:ex_doc, ">= 0.0.0", only: :docs, runtime: false},
+      {:inch_ex, "~> 0.2", only: :docs, runtime: false},
+      {:phoenix_html, "~> 2.10"},
+      {:plug, "~> 1.4"},
+      {:postgrex, "~> 0.14.0", only: [:test]}
+    ]
   end
 
   defp path(:test) do
     ["lib", "test/support", "test/fixtures"]
   end
+
   defp path(_), do: ["lib"]
 
   defp package do
-    [maintainers: ["Ally Raza"],
-     licenses: ["MIT"],
-     links: %{github: "https://github.com/elixirdrops/kerosene"},
-     files: ~w(lib test config) ++
-            ~w(CHANGELOG.md LICENSE.md mix.exs README.md)]
+    [
+      description: "Pagination for Ecto and Phoenix.",
+      maintainers: ["Ally Raza"],
+      licenses: ["MIT"],
+      files:
+        ~w(lib test config) ++
+          ~w(CHANGELOG.md LICENSE.md mix.exs README.md),
+      links: %{
+        GitHub: @source_url
+      }
+    ]
   end
 
-  def aliases do
-    [test: ["ecto.create --quite", "ecto.migrate --quite", "test"]]
+  defp aliases do
+    [
+      test: ["ecto.create --quite", "ecto.migrate --quite", "test"]
+    ]
+  end
+
+  defp preferred_cli_env do
+    [
+      docs: :docs
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        "CHANGELOG.md": [title: "Changelog"],
+        "LICENSE.md": [title: "License"],
+        "README.md": [title: "Overview"]
+      ],
+      main: "readme",
+      source_ref: "v#{@version}",
+      formatters: ["html"]
+    ]
   end
 end
