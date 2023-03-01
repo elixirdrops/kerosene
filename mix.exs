6cf52f0d650102e6dbe255c0e5a9e6ecac53446c
Max Strother <max@confly.pl>
2023-03-01 02:24:58 +0100

    Add a formatter and format the project

--- mix.exs
+++ mix.exs
@@ -3,29 +3,32 @@ defmodule Kerosene.Mixfile do
   @version "0.9.0"
 
   def project do
-    [app: :kerosene,
-     version: @version,
-     elixir: "~> 1.2",
-     elixirc_paths: path(Mix.env),
-     package: package(),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     aliases: aliases(),
-     name: "Kerosene",
-     docs: [source_ref: "v#{@version}", main: "Kerosene"],
-     source_url: "https://github.com/elixirdrops/kerosene",
-     description: """
-     Pagination for Ecto and Phoenix.
-     """]
+    [
+      app: :kerosene,
+      version: @version,
+      elixir: "~> 1.2",
+      elixirc_paths: path(Mix.env()),
+      package: package(),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      aliases: aliases(),
+      name: "Kerosene",
+      docs: [source_ref: "v#{@version}", main: "Kerosene"],
+      source_url: "https://github.com/elixirdrops/kerosene",
+      description: """
+      Pagination for Ecto and Phoenix.
+      """
+    ]
   end
 
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: application(Mix.env)]
+    [applications: application(Mix.env())]
   end
+
   defp application(:test), do: [:postgrex, :ecto, :logger]
   defp application(_), do: [:logger]
 
@@ -38,30 +41,36 @@ defmodule Kerosene.Mixfile do
   #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
   #
   # Type "mix help deps" for more examples and options
-  defp deps do
-    [{:phoenix_html, "~> 3.3"},
-     {:plug, "~> 1.7"},
-     {:ecto, "~> 3.0"},
-     {:ecto_sql, "~> 3.0"},
-     # Test dependencies
-     {:postgrex, "~> 0.14.0", only: [:test]},
-     # Docs dependencies
-     {:earmark, "~> 0.1", only: :docs},
-     {:ex_doc, "~> 0.11", only: :docs},
-     {:inch_ex, "~> 0.2", only: :docs}]
+  def deps do
+    [
+      {:phoenix_html, "~> 3.3"},
+      {:plug, "~> 1.7"},
+      {:ecto, "~> 3.0"},
+      {:ecto_sql, "~> 3.0"},
+      # Test dependencies
+      {:postgrex, "~> 0.14.0", only: [:test]},
+      # Docs dependencies
+      {:earmark, "~> 0.1", only: :docs},
+      {:ex_doc, "~> 0.11", only: :docs},
+      {:inch_ex, "~> 0.2", only: :docs}
+    ]
   end
 
   defp path(:test) do
     ["lib", "test/support", "test/fixtures"]
   end
+
   defp path(_), do: ["lib"]
 
   defp package do
-    [maintainers: ["Ally Raza"],
-     licenses: ["MIT"],
-     links: %{github: "https://github.com/elixirdrops/kerosene"},
-     files: ~w(lib test config) ++
-            ~w(CHANGELOG.md LICENSE.md mix.exs README.md)]
+    [
+      maintainers: ["Ally Raza"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/elixirdrops/kerosene"},
+      files:
+        ~w(lib test config) ++
+          ~w(CHANGELOG.md LICENSE.md mix.exs README.md)
+    ]
   end
 
   def aliases do
